75a213cce8e1f134045b36af1041d1d22058345e
Tommaso Bufano <tbufano@thinkseed.com>
2016-05-03 20:36:38 -0500

    Updated with live API data.

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -2,45 +2,46 @@ require 'twitter'
 
 class ResumesController < ApplicationController
   def index
-    @resume = Resume.new(
-      {
-        id: 1,
-        first_name: "Anna", 
-        last_name: "Gulstine",
-        email: "annagulstine@gmail.com",
-        phone_number: 9324893284,
-        short_bio: "lkdjfaldksfj",
-        linkedin_url: "ldjafljds",
-        twitter_handle: "dlkfadslkf",
-        blog: "ldkjfaldkjf",
-        resume_url: "ldkfjadlfj",
-        github_url: "ldjalkdjf",
-        photo: "ldkjfadlskfj",
-        experiences: [
-          {
-            id: 1,
-            job_title: "dlkfjasdlkf",
-            company_name: "lkdafjd",
-            start_date: "fjdaslkfj",
-            end_date: "ldkajfdlk",
-            details: "lakdfjd"
-          }
-        ],
-        educations: [
-          {
-            id: 1,
-            degree: "lkdafjd",
-            university_name: "ldakfjd",
-            start_date: "lkajdflkdsj",
-            end_date: "lkdsjaf",
-            details: "lkdajf"
-          }
-        ],
-        skills: [
-          id: 1,
-          name: "ldkajf"
-        ]
-      })
+    @resumes = Resume.all
+    # @resume = Resume.new(
+    #   {
+    #     id: 1,
+    #     first_name: "Anna", 
+    #     last_name: "Gulstine",
+    #     email: "annagulstine@gmail.com",
+    #     phone_number: 9324893284,
+    #     short_bio: "lkdjfaldksfj",
+    #     linkedin_url: "ldjafljds",
+    #     twitter_handle: "dlkfadslkf",
+    #     blog: "ldkjfaldkjf",
+    #     resume_url: "ldkfjadlfj",
+    #     github_url: "ldjalkdjf",
+    #     photo: "ldkjfadlskfj",
+    #     experiences: [
+    #       {
+    #         id: 1,
+    #         job_title: "dlkfjasdlkf",
+    #         company_name: "lkdafjd",
+    #         start_date: "fjdaslkfj",
+    #         end_date: "ldkajfdlk",
+    #         details: "lakdfjd"
+    #       }
+    #     ],
+    #     educations: [
+    #       {
+    #         id: 1,
+    #         degree: "lkdafjd",
+    #         university_name: "ldakfjd",
+    #         start_date: "lkajdflkdsj",
+    #         end_date: "lkdsjaf",
+    #         details: "lkdajf"
+    #       }
+    #     ],
+    #     skills: [
+    #       id: 1,
+    #       name: "ldkajf"
+    #     ]
+    #   })
   end
   
   def show
