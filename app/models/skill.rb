fe73a57d9bdbe69ccfec28177cfead3a7b790a5f
AnnaGulstine <annagulstine@gmail.com>
2016-05-04 00:29:33 -0500

    Fixed data displaying on show page

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill
   attr_reader :id, :name
 
   def initialize(resume_input)
-    @id = resume_input[:id]
-    @name = resume_input[:name]
+    @id = resume_input["id"]
+    @name = resume_input["name"]
   end
 end
